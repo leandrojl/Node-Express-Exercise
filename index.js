bbc62ecd8f73b8b90648e21a5bbd00fa23f8d28d
leandrojl <leandroracing96@hotmail.com>
2023-07-19 20:46:04 -0300

    Connect mustache and configure responses

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express'); //import express from node modules
 const path = require('path');
 const app = express(); // asign express to a variable
+const hoganMiddleware = require('hogan-middlware');
+
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'mustache');
-app.engine('mustache', require('hogan-middleware').__express);
+app.engine('mustache', hoganMiddleware.__express);
+
 //http get function
 app.get('/',(req, res, next)=>{
     res.send('This is a new server using Express');
@@ -19,6 +22,10 @@ app.get('/json',(req, res, next)=>{
         colleague: new String("Harvard")
     }
     res.json(data);
+})
+
+app.get('/home',(req, res, next)=>{
+    res.render('home', null);
 })
 
 app.listen(3000); //3000 its a port number
