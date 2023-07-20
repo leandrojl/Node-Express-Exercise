20d9e94391e88bc42e81636f2c717aaaf510c980
leandrojl <leandroracing96@hotmail.com>
2023-07-19 21:53:06 -0300

    Console log in node server

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express'); //import express from node modules
 const path = require('path');
 const app = express(); // asign express to a variable
-const hoganMiddleware = require('hogan-middlware');
+const hoganMiddleware = require('hogan-middleware');
 
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'mustache');
 app.engine('mustache', hoganMiddleware.__express);
+app.use(express.static(path.join(__dirname, 'public'))); //connected the statis assets to express
 
 //http get function
 app.get('/',(req, res, next)=>{
@@ -29,3 +30,5 @@ app.get('/home',(req, res, next)=>{
 })
 
 app.listen(3000); //3000 its a port number
+console.log('Im in the console');
+console.log('Server running on http://localhost:3000'); //to check that the server is properly running
